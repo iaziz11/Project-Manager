3147aff53de09c3865aae9b98222fa3eabd1c545
Imran Aziz <imrannoah11@gmail.com>
2023-04-04 19:02:58 -0700

    Initial commit

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from datetime import datetime
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/static/uploads/'
 
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
